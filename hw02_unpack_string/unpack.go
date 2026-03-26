b30b5af591e064c0057fdd9ed183968939d21920
Иван Липовцев <i.lipovtsev@automacon.ru>
2026-03-26 19:41:37 +0300

    Results of HW #2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -20,35 +20,22 @@ func Unpack(unpackingString string) (string, error) {
 			return "", ErrInvalidString
 		}
 		if current == 92 && i+1 < len(runes) {
-			// оббработка "/"
-			if unicode.IsLetter(runes[i+1]) {
-				// если после "/" буква - ошибка
-				return "", ErrInvalidString
-			} else if i+2 < len(runes) && runes[i+1] == 92 && unicode.IsDigit(runes[i+2]) {
-				// если после "/" стоит "/" и после него цифра
-				if unicode.IsDigit(runes[i+2]) {
-					num := int(runes[i+2] - '0')
-					for j := 0; j < num; j++ {
-						result = append(result, current)
-					}
-					i += 2
-				}
-			} else if runes[i+1] == 92 {
-				// если после "/" стоит "/" и дальше не цифра
-				result = append(result, current)
-				i++
+			// обработка "/"
+			newI, err := handleBackslash(runes, i, &result)
+			if err != nil {
+				return "", err
 			}
+			i = newI
 			continue
 		}
 		if i > 0 && unicode.IsDigit(current) && unicode.IsDigit(runes[i-1]) && runes[i-2] != 92 {
-			// если две цифры рялом и нет экранирования
+			// если две цифры рядом и нет экранирования
 			return "", ErrInvalidString
 		}
 		if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
 			// если после символа цифра
 			num := int(runes[i+1] - '0')
-			if num == 0 {
-			} else {
+			if num != 0 {
 				for j := 0; j < num; j++ {
 					result = append(result, current)
 				}
@@ -60,4 +47,31 @@ func Unpack(unpackingString string) (string, error) {
 		}
 	}
 	return string(result), nil
+}
+
+func handleBackslash(runes []rune, i int, result *[]rune) (int, error) {
+	current := runes[i]
+
+	switch {
+	case unicode.IsLetter(runes[i+1]):
+		// если после "/" буква - ошибка
+		return i, ErrInvalidString
+
+	case i+2 < len(runes) && runes[i+1] == 92 && unicode.IsDigit(runes[i+2]):
+		// если после "/" стоит "/" и после него цифра
+		num := int(runes[i+2] - '0')
+		for j := 0; j < num; j++ {
+			*result = append(*result, current)
+		}
+		return i + 2, nil
+
+	case runes[i+1] == 92:
+		// если после "/" стоит "/" и дальше не цифра
+		*result = append(*result, current)
+		return i + 1, nil
+
+	default:
+		// если после "/" что-то другое
+		return i, nil
+	}
 }
